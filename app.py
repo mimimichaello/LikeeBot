83a743f743a56226de03e178f00e0f113bdae7e2
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-04-01 18:28:16 +0300

    moved handlers to a separate folder and created a command menu

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 ﻿import asyncio
 import os
-from aiogram import Bot, Dispatcher
-# from dotenv import find_dotenv, load_dotenv
-# load_dotenv(find_dotenv())
-
-from handlers.user_private import user_private_router
 from settings.config import TOKEN
+from aiogram import Bot, Dispatcher, types
+from handlers.user_private import user_private_router
+from common.bot_cmds_list import private
 
 
 ALLOWED_UPDATES = ["message, edited_message"]
@@ -18,6 +16,7 @@ dp.include_router(user_private_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,5 +1,6 @@
 ﻿from aiogram import types, Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+from aiogram.enums import ParseMode
 
 
 user_private_router = Router()
@@ -14,12 +15,35 @@ async def start_cmd(message: types.Message):
     if user_id not in users_started:
         users_started.add(user_id)
         await message.answer(
-            f"Привет, {message.from_user.first_name}! Для начала внимательно изучи вкладки Навигация и Инструкция!"
+            f"Привет, {message.from_user.first_name}! \nДля начала внимательно изучите вкладки <b>Навигация</b> и <b>Инструкция</b>! \nЧтобы открыть меню, введите команду /menu.",
+            parse_mode=ParseMode.HTML,
         )
     else:
-        await message.answer(f"{message.from_user.first_name}, выберите опцию")
+        await message.answer(f"{message.from_user.first_name}, выберите опцию. \nЧтобы открыть меню, введите команду /menu.")
 
 
-@user_private_router.message()
-async def answer(message: types.Message):
-    await message.answer("Ответ")
+@user_private_router.message(Command("menu"))
+async def menu_cmd(message: types.Message):
+    await message.answer("Вот меню")
+
+# @user_private_router.message(Command("active"))
+# async def active_cmd(message: types.Message):
+#     await message.answer("Актив")
+
+# @user_private_router.message(Command("navigation"))
+# async def active_cmd(message: types.Message):
+#     await message.answer("Навигация")
+
+# @user_private_router.message(Command("instruction"))
+# async def instruction_cmd(message: types.Message):
+#     await message.answer("Инструкция")
+
+# @user_private_router.message(Command("rules"))
+# async def rules_cmd(message: types.Message):
+#     await message.answer("Правила")
+
+# @user_private_router.message(Command("payment"))
+# async def payment_cmd(message: types.Message):
+#     await message.answer("Купить публикацию")
+
+
