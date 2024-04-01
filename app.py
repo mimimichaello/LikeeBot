8b0d8c08f8b67390196b00e16f5b6ecf5885dc8e
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-04-01 13:54:01 +0300

    initial

---
+++ app.py
@@ -0,0 +1,24 @@
+﻿import asyncio
+import os
+from aiogram import Bot, Dispatcher
+# from dotenv import find_dotenv, load_dotenv
+# load_dotenv(find_dotenv())
+
+from handlers.user_private import user_private_router
+from settings.config import TOKEN
+
+
+ALLOWED_UPDATES = ["message, edited_message"]
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+dp.include_router(user_private_router)
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+
+
+asyncio.run(main())
---
+++ handlers/user_private.py
@@ -0,0 +1,25 @@
+﻿from aiogram import types, Router
+from aiogram.filters import CommandStart
+
+
+user_private_router = Router()
+
+user_last_submission = {}
+users_started = set()
+
+
+@user_private_router.message(CommandStart())
+async def start_cmd(message: types.Message):
+    user_id = message.from_user.id
+    if user_id not in users_started:
+        users_started.add(user_id)
+        await message.answer(
+            f"Привет, {message.from_user.first_name}! Для начала внимательно изучи вкладки Навигация и Инструкция!"
+        )
+    else:
+        await message.answer(f"{message.from_user.first_name}, выберите опцию")
+
+
+@user_private_router.message()
+async def answer(message: types.Message):
+    await message.answer("Ответ")
