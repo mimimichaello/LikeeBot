1d3d6fc3255cee4712c7651c7aee1d0aca63276d
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-04-02 13:39:31 +0300

    Created get_keyboard and utils_reply

---
+++ keyboards/utils_reply.py
@@ -0,0 +1,25 @@
+﻿from keyboards.get_keyboard import get_keyboard
+
+
+def start_menu():
+    return get_keyboard(
+        "Навигация",
+        "Инструкция",
+        "Правила",
+        "Актив",
+        "Купить публикацию",
+        "FAQ",
+        placeholder="Что вас интересует?",
+        sizes=(3, 2, 1),
+    )
+
+
+def admin_kb():
+    return get_keyboard(
+        "Добавить подписку",
+        "Изменить подписку",
+        "Удалить подписку",
+        "Я так, просто посмотреть зашел",
+        placeholder="Выберите действие",
+        sizes=(2, 1, 1),
+    )
