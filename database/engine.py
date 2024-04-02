0ed0997385c0b69ac6975e2aa7bc175c6e6c0193
mimimichaello <137781824+mimimichaello@users.noreply.github.com>
2024-04-02 21:59:33 +0300

    Created engine

---
+++ database/engine.py
@@ -0,0 +1,21 @@
+﻿import os
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from database.models import BaseSubscribe
+
+
+engine = create_async_engine(os.getenv("DB_LITE"), echo=True)
+
+session_maker = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseSubscribe.metadata.create_all)
+
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseSubscribe.metadata.drop_all)
